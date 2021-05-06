facbfa956ee3ee0a564b17ed022a8c66e9035661
Emma Dixon <emma.dixon@diamond.ac.uk>
2021-05-06 15:07:25 +0100

    Rename kafka_consumer

--- kafka_consumer/__init__.py
+++ kafka_consumer/__init__.py
@@ -1,4 +1,4 @@
 from kafka_consumer._version_git import __version__
-from kafka_consumer.kafka_consumer import consume_and_write
+from kafka_consumer.consume_and_write import consume_and_write
 
 __all__ = ["__version__", "consume_and_write"]
