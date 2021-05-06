65b6e80c6d643c52ae5ceac8c981a9bd4fe6deb3
Emma Dixon <emma.dixon@diamond.ac.uk>
2021-05-06 10:51:15 +0100

    WIP: Workaround for import not found

--- kafka_consumer/FB_Tables/NDArray.py
+++ kafka_consumer/FB_Tables/NDArray.py
@@ -47,7 +47,7 @@ class NDArray(object):
         o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
         if o != 0:
             x = o + self._tab.Pos
-            from FB_Tables.epicsTimeStamp import epicsTimeStamp
+            from kafka_consumer.FB_Tables.epicsTimeStamp import epicsTimeStamp
             obj = epicsTimeStamp()
             obj.Init(self._tab.Bytes, x)
             return obj
@@ -121,7 +121,7 @@ class NDArray(object):
             x = self._tab.Vector(o)
             x += flatbuffers.number_types.UOffsetTFlags.py_type(j) * 4
             x = self._tab.Indirect(x)
-            from FB_Tables.NDAttribute import NDAttribute
+            from kafka_consumer.FB_Tables.NDAttribute import NDAttribute
             obj = NDAttribute()
             obj.Init(self._tab.Bytes, x)
             return obj
