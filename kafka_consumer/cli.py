129c2b8f45504d3f2d6c58db1f4b1be8025349b3
Emma Dixon <emma.dixon@diamond.ac.uk>
2021-05-12 16:49:28 +0100

    Add comsume from timestamp

--- kafka_consumer/cli.py
+++ kafka_consumer/cli.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from time import time
 
 from kafka_consumer import __version__, consume_and_write
 
@@ -9,6 +10,14 @@ def main(args=None):
     parser.add_argument("broker", type=str, help="Broker")
     parser.add_argument("group", type=str, help="Group")
     parser.add_argument("topic", type=str, help="Topic")
+    parser.add_argument(
+        "-t",
+        "--timestamp",
+        type=int,
+        help="Timestamp as secs since epoch to start consuming from",
+        required=False,
+        default=time(),
+    )
     args = parser.parse_args(args)
     consume_and_write(
         args.broker,
@@ -17,4 +26,5 @@ def main(args=None):
         "/dls/science/users/wqt58532/kafka_consumer",
         "test_consume.h5",
         50,
+        args.timestamp,
     )
--- kafka_consumer/consume_and_write.py
+++ kafka_consumer/consume_and_write.py
@@ -1,12 +1,32 @@
 import sys
-import time
+from time import time
 
-from confluent_kafka import Consumer, KafkaException
+from confluent_kafka import Consumer, KafkaException, TopicPartition
+from confluent_kafka.admin import AdminClient
 
 from kafka_consumer.h5_file import H5File
 
 
-def consume_and_write(broker, group, topics, filepath, filename, num_arrays):
+def get_num_partitions_for_topic(broker, topic):
+    conf = {"bootstrap.servers": broker}
+    kafka_admin = AdminClient(conf)
+    kafka_topic_metadata = kafka_admin.list_topics().topics
+    num_partitions = len(kafka_topic_metadata.get(topic).partitions)
+    return num_partitions
+
+
+def find_offsets_from_time(consumer, topic, num_partitions=1, secs_since_epoch=time()):
+    search_ms = secs_since_epoch * 1000 + 1
+    topic_partions_to_search = [
+        TopicPartition(topic, p, int(search_ms)) for p in range(num_partitions)
+    ]
+    offsets = consumer.offsets_for_times(topic_partions_to_search, timeout=1.0)
+    return offsets
+
+
+def consume_and_write(
+    broker, group, topic, filepath, filename, num_arrays, timestamp=time()
+):
     """Simple kafka consumer
 
     Args:
@@ -18,16 +38,22 @@ def consume_and_write(broker, group, topics, filepath, filename, num_arrays):
     def print_assignment(consumer, partitions):
         print("Assignment:", partitions)
 
+    num_partitions = get_num_partitions_for_topic(broker, topic)
+
     # Consumer configuration
     # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
     conf = {
         "bootstrap.servers": broker,
         "group.id": group,
         "session.timeout.ms": 6000,
-        "auto.offset.reset": "latest",
+        "auto.offset.reset": "earliest",
     }
     c = Consumer(conf)
-    c.subscribe([topics], on_assign=print_assignment)
+
+    offsets = find_offsets_from_time(c, topic, num_partitions, timestamp)
+    c.assign(offsets)
+    for offset in offsets:
+        c.seek(offset)
 
     h5file = H5File()
     h5file.create(filepath, filename, num_arrays)
@@ -48,7 +74,7 @@ def consume_and_write(broker, group, topics, filepath, filename, num_arrays):
                     f"Partition: [{msg.partition()}] "
                     f"Offset: {msg.offset()} "
                     f"Key: {msg.key()} "
-                    f"Time: {time.time()}"
+                    f"Time: {time()}"
                 )
                 h5file.add_array_from_flatbuffer(msg.value())
 
