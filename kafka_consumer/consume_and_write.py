608a4e208a20de4868fb642f4ab50df1124d02ff
Emma Dixon <emma.dixon@diamond.ac.uk>
2021-05-13 11:25:47 +0100

    Refactor into kafka_admin

--- kafka_consumer/consume_and_write.py
+++ kafka_consumer/consume_and_write.py
@@ -1,27 +1,10 @@
 import sys
 from time import time
 
-from confluent_kafka import Consumer, KafkaException, TopicPartition
-from confluent_kafka.admin import AdminClient
+from confluent_kafka import Consumer, KafkaException
 
 from kafka_consumer.h5_file import H5File
-
-
-def get_num_partitions_for_topic(broker, topic):
-    conf = {"bootstrap.servers": broker}
-    kafka_admin = AdminClient(conf)
-    kafka_topic_metadata = kafka_admin.list_topics().topics
-    num_partitions = len(kafka_topic_metadata.get(topic).partitions)
-    return num_partitions
-
-
-def find_offsets_from_time(consumer, topic, num_partitions=1, secs_since_epoch=time()):
-    search_ms = secs_since_epoch * 1000 + 1
-    topic_partions_to_search = [
-        TopicPartition(topic, p, int(search_ms)) for p in range(num_partitions)
-    ]
-    offsets = consumer.offsets_for_times(topic_partions_to_search, timeout=1.0)
-    return offsets
+from kafka_consumer.kafka_admin import num_partitions_for_topic, offsets_after_time
 
 
 def consume_and_write(
@@ -38,7 +21,7 @@ def consume_and_write(
     def print_assignment(consumer, partitions):
         print("Assignment:", partitions)
 
-    num_partitions = get_num_partitions_for_topic(broker, topic)
+    num_partitions = num_partitions_for_topic(broker, topic)
 
     # Consumer configuration
     # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
@@ -50,7 +33,7 @@ def consume_and_write(
     }
     c = Consumer(conf)
 
-    offsets = find_offsets_from_time(c, topic, num_partitions, timestamp)
+    offsets = offsets_after_time(c, topic, num_partitions, timestamp)
     c.assign(offsets)
     for offset in offsets:
         c.seek(offset)
